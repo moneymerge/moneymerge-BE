eff2d76436a6dec1b9a5db08922cd5206d0fd7f4
leeyejin <leeyejin113@gmail.com>
2024-03-25 12:51:48 +0900

    Initial commit

---
+++ src/main/java/example/com/meneymergebe/MeneymergeBeApplication.java
@@ -0,0 +1,13 @@
+package example.com.meneymergebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeneymergeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeneymergeBeApplication.class, args);
+    }
+
+}
