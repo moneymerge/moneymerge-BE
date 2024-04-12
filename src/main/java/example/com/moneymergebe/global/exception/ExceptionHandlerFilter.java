93b4340b2de901b162874887a5d6ffe1c46bc220
leeyejin <leeyejin113@gmail.com>
2024-04-12 13:44:51 +0900

    [#5] Feat: ExceptionHandlerFilter 구현

---
+++ src/main/java/example/com/moneymergebe/global/exception/ExceptionHandlerFilter.java
@@ -0,0 +1,42 @@
+package example.com.moneymergebe.global.exception;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import example.com.moneymergebe.global.response.CommonResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import org.springframework.http.MediaType;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+public class ExceptionHandlerFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(
+        HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+        throws ServletException, IOException {
+
+        try {
+            filterChain.doFilter(request, response);
+        } catch (GlobalException e) {
+            setErrorResponse(response, e.getResultCode());
+        }
+    }
+
+    private void setErrorResponse(HttpServletResponse response, ResultCode resultCode) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.setStatus(resultCode.getStatus().value());
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        try {
+            response.getWriter().write(objectMapper.writeValueAsString(
+                new CommonResponse<>(resultCode.getStatus().value(), resultCode.getMessage())));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
