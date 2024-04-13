b07e6408b7a3a14afcdfe80eb5db2b033261e08b
leeyejin <leeyejin113@gmail.com>
2024-04-13 16:37:33 +0900

    [#10] Feat: User entity 임시 추가

--- src/main/java/example/com/moneymergebe/domain/user/entity/User.java
+++ src/main/java/example/com/moneymergebe/domain/user/entity/User.java
@@ -1,10 +1,17 @@
 package example.com.moneymergebe.domain.user.entity;
 
+import example.com.moneymergebe.domain.book.entity.BookUser;
+import example.com.moneymergebe.domain.common.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,17 +20,29 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "tb_user")
-public class User {
+public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long userId;
 
     private String username; // 닉네임
 
     private String email;
 
-    private String profileUrl;
+    private String profileUrl; // 프로필 사진 URL
 
-    private UserRole role;
+    // TODO: 소셜 종류 추가
+
+    private Long characterId; // 현재 캐릭터
+
+    private Long point;
+
+    private boolean alarm; // 알람 설정
+
+    @Enumerated(value = EnumType.STRING)
+    private UserRole role; // 권한
+
+    @OneToMany(mappedBy = "user")
+    private List<BookUser> bookUserList = new ArrayList<>();
 
 }
