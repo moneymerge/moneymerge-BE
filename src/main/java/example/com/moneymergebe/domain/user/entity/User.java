e09d5a913f5e1f44777ebff96bdaaa5a9445144e
young5306 <harrysunae@naver.com>
2024-04-15 09:40:53 +0900

    Revert "[#3] Feat: User, UserSocialEnum 추가"
    This reverts commit 72a46c4397a42ec47974831b06a18e8f2e06ac56.

--- src/main/java/example/com/moneymergebe/domain/user/entity/User.java
+++ src/main/java/example/com/moneymergebe/domain/user/entity/User.java
@@ -1,14 +1,13 @@
 package example.com.moneymergebe.domain.user.entity;
 
-import example.com.moneymergebe.domain.user.entity.UserRole;
-import example.com.moneymergebe.domain.user.entity.UserSocialEnum;
-import jakarta.persistence.*;
-import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -19,51 +18,12 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable=false, unique=true)
     private String nickname;
 
-    @Column(nullable=false)
-    private String password;
-
-    @Column(nullable=false)
     private String email;
 
-    @Column(nullable=false)
     private String profileUrl;
 
-    @Enumerated(EnumType.STRING)
     private UserRole role;
 
-    @Enumerated(EnumType.STRING)
-    private UserSocialEnum social;
-
-    @Column
-    private int character;
-
-    @Column
-    private int point;
-
-//    @Column
-//    private int alarm;
-
-    @Column
-    @CreationTimestamp
-    private LocalDateTime created_at = LocalDateTime.now();
-
-    @Column
-    @UpdateTimestamp
-    private LocalDateTime updated_at = LocalDateTime.now();
-
-
-    @Builder
-    public User(String nickname, String password, String email, String profileUrl, UserRole role, UserSocialEnum social, int character, int point){
-        this.nickname=nickname;
-        this.password=password;
-        this.email=email;
-        this.profileUrl=profileUrl;
-        this.role=role;
-        this.social=social;
-        this.character=character;
-        this.point=point;
-    }
 }
