3d2251ce507783388c98a5b7f4f93c48a8ee919a
young5306 <harrysunae@naver.com>
2024-04-16 11:48:36 +0900

    [#3] Feat: 카카오 로그인

---
+++ src/main/java/example/com/moneymergebe/domain/user/dto/request/KakaoInsertReq.java
@@ -0,0 +1,48 @@
+package example.com.moneymergebe.domain.user.dto.request;
+
+import com.nimbusds.jose.shaded.gson.JsonElement;
+import java.util.HashMap;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class KakaoInsertReq {
+    private String email;
+    private String nickname;
+    private String profileUrl;
+    private String accessToken;
+    private String refreshToken;
+
+    public static KakaoInsertReq of(JsonElement element, HashMap<String, String> tokens) {
+        return KakaoInsertReq.builder()
+            .email(
+                element
+                    .getAsJsonObject()
+                    .get("kakao_account")
+                    .getAsJsonObject()
+                    .get("email")
+                    .getAsString())
+            .nickname(
+                element
+                    .getAsJsonObject()
+                    .get("kakao_account")
+                    .getAsJsonObject()
+                    .get("profile")
+                    .getAsJsonObject()
+                    .get("nickname")
+                    .getAsString())
+            .profileUrl(
+                element
+                    .getAsJsonObject()
+                    .get("kakao_account")
+                    .getAsJsonObject()
+                    .get("profile")
+                    .getAsJsonObject()
+                    .get("profile_image_url")
+                    .getAsString())
+            .accessToken(tokens.get("accessToken"))
+            .refreshToken(tokens.get("refreshToken"))
+            .build();
+    }
+}
