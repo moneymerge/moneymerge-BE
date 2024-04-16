afaffcd260814652cf82e464967c9cd3c470a425
young5306 <harrysunae@naver.com>
2024-04-16 10:54:37 +0900

    [#3] Feat: UserController

---
+++ src/main/java/example/com/moneymergebe/domain/user/controller/UserController.java
@@ -0,0 +1,44 @@
+package example.com.moneymergebe.domain.user.controller;
+
+import example.com.moneymergebe.domain.user.service.UserService;
+import example.com.moneymergebe.global.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
+    private String kakaoClientId;
+    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
+    private String kakaoRedirectUri;
+    @Value("${spring.security.oauth2.client.provider.kakao.authorization-uri}")
+    private String kakaoAuthorizationUri;
+
+
+    private final UserService userService;
+    private final JwtUtil jwtUtil;
+    //private final RedisUtil redisUtil;
+
+    private static final String LOGOUT_VALUE = "logout";
+
+    @GetMapping("/login-page")
+    public String loginPage(Model model) {
+        model.addAttribute("kakaoAuthorizationUri", kakaoAuthorizationUri);
+        model.addAttribute("kakaoClientId", kakaoClientId);
+        model.addAttribute("kakaoRedirectUri", kakaoRedirectUri);
+        return "login.html";
+    }
+
+    @GetMapping("/signup-page")
+    public String signupPage() {
+        return "signup.html";
+    }
+
+}
---
+++ src/main/java/example/com/moneymergebe/domain/user/service/UserService.java
@@ -0,0 +1,8 @@
+package example.com.moneymergebe.domain.user.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+}
