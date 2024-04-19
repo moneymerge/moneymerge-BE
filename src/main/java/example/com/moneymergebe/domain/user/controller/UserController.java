da96dc5a26ad162304fa93858da9eff04aa6ea3b
young5306 <harrysunae@naver.com>
2024-04-20 01:00:07 +0900

    [#3] Feat: login

--- src/main/java/example/com/moneymergebe/domain/user/controller/UserController.java
+++ src/main/java/example/com/moneymergebe/domain/user/controller/UserController.java
@@ -8,22 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("")
 @RequiredArgsConstructor
 public class UserController {
-
-    @Value("${spring.security.oauth2.client.registration.kakao.client-id}")
-    private String kakaoClientId;
-    @Value("${spring.security.oauth2.client.registration.kakao.redirect-uri}")
-    private String kakaoRedirectUri;
-    @Value("${spring.security.oauth2.client.provider.kakao.authorization-uri}")
-    private String kakaoAuthorizationUri;
-
     @GetMapping("/api/users/login-page")
-    public String loginPage(Model model) {
-        model.addAttribute("kakaoAuthorizationUri", kakaoAuthorizationUri);
-        model.addAttribute("kakaoClientId", kakaoClientId);
-        model.addAttribute("kakaoRedirectUri", kakaoRedirectUri);
+    public String loginPage() {
         return "login";
     }
 }
--- src/main/java/example/com/moneymergebe/domain/user/repository/UserRepository.java
+++ src/main/java/example/com/moneymergebe/domain/user/repository/UserRepository.java
@@ -4,5 +4,5 @@ import example.com.moneymergebe.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    User findByEmail(String email);
 }
