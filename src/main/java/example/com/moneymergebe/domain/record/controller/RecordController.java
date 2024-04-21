779f1dba14b7180fe9dedaccf128709bc147070f
leeyejin <leeyejin113@gmail.com>
2024-04-21 19:05:51 +0900

    [#17] Feat: 월 레코드 조회

--- src/main/java/example/com/moneymergebe/domain/record/controller/RecordController.java
+++ src/main/java/example/com/moneymergebe/domain/record/controller/RecordController.java
@@ -1,12 +1,16 @@
 package example.com.moneymergebe.domain.record.controller;
 
 import example.com.moneymergebe.domain.record.dto.request.RecordSaveReq;
+import example.com.moneymergebe.domain.record.dto.response.RecordGetMonthRes;
 import example.com.moneymergebe.domain.record.dto.response.RecordSaveRes;
 import example.com.moneymergebe.domain.record.service.RecordService;
 import example.com.moneymergebe.global.response.CommonResponse;
 import example.com.moneymergebe.global.security.UserDetailsImpl;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,5 +33,20 @@ public class RecordController {
         req.setUserId(userDetails.getUser().getUserId());
         return CommonResponse.success(recordService.saveRecord(req));
     }
+
+    /**
+     * 월 레코드 조회
+     * @param userDetails 사용자 정보
+     * @param bookId 가계부 ID
+     * @param year 조회 연도
+     * @param month 조회 월
+     * @return 해당 날짜의 레코드 리스트
+     */
+    @GetMapping("{bookId}/{year}/{month}")
+    public CommonResponse<List<RecordGetMonthRes>> getMonthRecords(@AuthenticationPrincipal UserDetailsImpl userDetails,
+        @PathVariable Long bookId, @PathVariable int year, @PathVariable int month) {
+        return CommonResponse.success(recordService.getMonthRecords(userDetails.getUser().getUserId(), bookId, year, month));
+    }
+
 
 }
--- src/main/java/example/com/moneymergebe/domain/record/service/RecordService.java
+++ src/main/java/example/com/moneymergebe/domain/record/service/RecordService.java
@@ -5,6 +5,7 @@ import example.com.moneymergebe.domain.book.entity.BookUser;
 import example.com.moneymergebe.domain.book.repository.BookRepository;
 import example.com.moneymergebe.domain.book.repository.BookUserRepository;
 import example.com.moneymergebe.domain.record.dto.request.RecordSaveReq;
+import example.com.moneymergebe.domain.record.dto.response.RecordGetMonthRes;
 import example.com.moneymergebe.domain.record.dto.response.RecordSaveRes;
 import example.com.moneymergebe.domain.record.entity.Record;
 import example.com.moneymergebe.domain.record.repository.RecordRepository;
@@ -14,6 +15,8 @@ import example.com.moneymergebe.global.exception.GlobalException;
 import example.com.moneymergebe.global.validator.BookUserValidator;
 import example.com.moneymergebe.global.validator.BookValidator;
 import example.com.moneymergebe.global.validator.UserValidator;
+import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,12 +37,29 @@ public class RecordService {
         User user = findUser(req.getUserId());
         for(Long bookId : req.getBookList()) {
             Book book = findBook(bookId);
-            checkBookMember(user, book);
+            checkBookMember(user, book); // 가계부 권한 검사
             Record record = Record.builder().date(req.getDate()).recordType(req.getRecordType()).amount(req.getAmount()).assetType(req.getAssetType())
                 .content(req.getContent()).memo(req.getMemo()).image(req.getImage()).book(book).user(user).build(); // TODO: 카테고리 추가
             recordRepository.save(record);
         }
         return new RecordSaveRes();
+    }
+
+    /**
+     * 월 레코드 조회
+     */
+    @Transactional(readOnly = true)
+    public List<RecordGetMonthRes> getMonthRecords(Long userId, Long bookId, int year, int month) {
+        User user = findUser(userId);
+        Book book = findBook(bookId);
+        BookUser bookUser = checkBookMember(user, book); // 가계부 권한 검사
+
+        LocalDate startDate = LocalDate.of(year, month, 1); // 시작일
+        LocalDate endDate = startDate.withDayOfMonth(startDate.lengthOfMonth()); // 마지막일
+
+        return recordRepository.findAllByBookAndDateBetweenOrderByDate(book, startDate, endDate).stream().map(
+            record -> new RecordGetMonthRes(record, bookUser)
+        ).toList();
     }
 
     /**
@@ -63,8 +83,9 @@ public class RecordService {
     /**
      * @throws GlobalException user가 book의 멤버가 아닌 경우 예외 발생
      */
-    private void checkBookMember(User user, Book book) {
+    private BookUser checkBookMember(User user, Book book) {
         BookUser bookUser = bookUserRepository.findByUserAndBook(user, book);
         BookUserValidator.checkMember(bookUser);
+        return bookUser;
     }
 }
