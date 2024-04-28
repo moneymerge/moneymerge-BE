1bddbd63828c49fef6f3c67fbfdec3d74ac6e48f
leeyejin <leeyejin113@gmail.com>
2024-04-28 21:41:49 +0900

    [#18] Feat: 영수증 관련 entity 추가

---
+++ src/main/java/example/com/moneymergebe/domain/receipt/entity/ReceiptLike.java
@@ -0,0 +1,37 @@
+package example.com.moneymergebe.domain.receipt.entity;
+
+import example.com.moneymergebe.domain.common.BaseEntity;
+import example.com.moneymergebe.domain.user.entity.User;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tb_receipt_like")
+public class ReceiptLike extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long receiptLikeId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "receipt_id")
+    private Receipt receipt;
+
+    public ReceiptLike(User user, Receipt receipt) {
+        this.user = user;
+        this.receipt = receipt;
+    }
+}
