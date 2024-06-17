9a93a4a3b335fd36c2b2784fbb447db0e0e45126
leeyejin <leeyejin113@gmail.com>
2024-06-17 18:47:26 +0900

    Fix: cors 수정

--- src/main/java/example/com/moneymergebe/global/webMVC/WebMvcConfig.java
+++ src/main/java/example/com/moneymergebe/global/webMVC/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-            .allowedOrigins("http://cdpn.io", "http://localhost:3000", "http://43.203.66.36:3000")
+            .allowedOrigins("http://cdpn.io", "http://localhost:3000", "https://moneymerge.store")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true);
